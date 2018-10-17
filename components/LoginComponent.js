fecb46cb27661cbcfbb61940209a2ed2ed089c53
DuanVH <vuduanuet@gmail.com>
2018-10-17 15:03:03 +0700

    Get start Firebase

---
+++ components/LoginComponent.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {
+    Dimensions, Platform, StyleSheet,
+    View, Text, Image, TextInput,
+} from 'react-native';
+
+export default class LoginComponent extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <View/>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    containerStyle: {
+        flw
+    },
+});
